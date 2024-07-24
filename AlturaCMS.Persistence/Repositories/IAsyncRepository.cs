1f2524629767246f6ce53c41d7334cd5f2c29464
mheidari988 <m.heidari988@gmail.com>
2024-07-24 16:57:02 +0300

    feat: Add IAsyncRepository interface for asynchronous data operations
    https://heidari.atlassian.net/browse/CMS-10
    
    - Added IAsyncRepository<T> interface in AlturaCMS.Persistence.Repositories namespace
    - Defined methods: GetByIdAsync, ListAllAsync, ListAsync, AddAsync, UpdateAsync, DeleteAsync
    - Ensured the interface works with entities derived from BaseEntity and supports specifications for queries

---
+++ AlturaCMS.Persistence/Repositories/IAsyncRepository.cs
@@ -0,0 +1,14 @@
+﻿using AlturaCMS.Domain.Common;
+using AlturaCMS.Domain.Specifications;
+
+namespace AlturaCMS.Persistence.Repositories;
+
+public interface IAsyncRepository<T> where T : BaseEntity
+{
+    Task<T?> GetByIdAsync(Guid id);
+    Task<IReadOnlyList<T>> ListAllAsync();
+    Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec);
+    Task<T> AddAsync(T entity);
+    Task UpdateAsync(T entity);
+    Task DeleteAsync(T entity);
+}
