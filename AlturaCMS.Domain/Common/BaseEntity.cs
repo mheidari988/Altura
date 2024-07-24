13e9979a078603a956cb39c7224e338ecba58ca4
mheidari988 <m.heidari988@gmail.com>
2024-07-24 16:54:28 +0300

    feat: Add BaseEntity class with common properties for domain entities
    - Added BaseEntity abstract class in AlturaCMS.Domain.Common namespace
    - Included properties: Id, CreatedDate, UpdatedDate, IsDeleted, DeletedDate, RowVersion, CreatedBy, UpdatedBy, DeletedBy
    - Applied data annotations for validation and concurrency control
    - Initialized default values in the constructor
    
    https://heidari.atlassian.net/browse/CMS-10

--- AlturaCMS.Domain/Common/BaseEntity.cs
+++ AlturaCMS.Domain/Common/BaseEntity.cs
@@ -1,10 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace AlturaCMS.Domain.Common;
-internal class BaseEntity
+
+public abstract class BaseEntity
 {
+    [Key]
+    public Guid Id { get; set; }
+
+    [Required]
+    public DateTime CreatedDate { get; set; }
+
+    public DateTime? UpdatedDate { get; set; }
+
+    public bool IsDeleted { get; set; }
+
+    public DateTime? DeletedDate { get; set; }
+
+    [Required]
+    [ConcurrencyCheck]
+    public byte[] RowVersion { get; set; }
+
+    [Required]
+    [MaxLength(450)]
+    public string CreatedBy { get; set; } = string.Empty;
+
+    [MaxLength(450)]
+    public string UpdatedBy { get; set; } = string.Empty;
+
+    [MaxLength(450)]
+    public string DeletedBy { get; set; } = string.Empty;
+
+    // Constructor to set default values
+    protected BaseEntity()
+    {
+        CreatedDate = DateTime.UtcNow;
+        IsDeleted = false;
+        RowVersion = [];
+    }
 }
