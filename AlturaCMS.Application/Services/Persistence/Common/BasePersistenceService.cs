98a1418f92fc2a2307bbacb94a760719371f2d1a
mheidari988 <m.heidari988@gmail.com>
2024-07-25 21:23:15 +0300

    feat: add IBasePersistenceService interface and BasePersistenceService abstract class
    https://heidari.atlassian.net/browse/CMS-10
    
    - Added IBasePersistenceService interface in AlturaCMS.Application.Services.Persistence.Common namespace
    - Added BasePersistenceService abstract class in AlturaCMS.Application.Services.Persistence.Common namespace
    
    The IBasePersistenceService interface provides methods for basic CRUD operations, criteria-based retrieval, and specification-based retrieval. The BasePersistenceService abstract class implements these methods using a unit of work pattern.

---
+++ AlturaCMS.Application/Services/Persistence/Common/BasePersistenceService.cs
@@ -0,0 +1,59 @@
+﻿using AlturaCMS.Domain.Common;
+using AlturaCMS.Domain.Specifications;
+using AlturaCMS.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore.Query;
+using System.Linq.Expressions;
+
+namespace AlturaCMS.Application.Services.Persistence.Common;
+public abstract class BasePersistenceService<T>(IUnitOfWork unitOfWork) : IBasePersistenceService<T> where T : BaseEntity
+{
+    protected readonly IUnitOfWork UnitOfWork = unitOfWork;
+
+    public async Task<T?> GetByIdAsync(Guid id)
+    {
+        return await UnitOfWork.Repository<T>().GetByIdAsync(id);
+    }
+
+    public async Task<IReadOnlyList<T>> GetAllAsync()
+    {
+        return await UnitOfWork.Repository<T>().ListAllAsync();
+    }
+
+    public async Task<IReadOnlyList<T>> GetByCriteriaAsync(
+        Expression<Func<T, bool>> predicate,
+        Func<IQueryable<T>, IIncludableQueryable<T, object>> include = null!,
+        Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null!,
+        int? skip = null,
+        int? take = null)
+    {
+        return await UnitOfWork.Repository<T>().ListAsync(predicate, include, orderBy, skip, take);
+    }
+
+    public async Task<IReadOnlyList<T>> GetBySpecificationAsync(ISpecification<T> spec)
+    {
+        return await UnitOfWork.Repository<T>().ListAsync(spec);
+    }
+
+    public async Task<T> CreateAsync(T entity)
+    {
+        var newEntity = await UnitOfWork.Repository<T>().AddAsync(entity);
+        await UnitOfWork.CompleteAsync();
+        return newEntity;
+    }
+
+    public async Task UpdateAsync(T entity)
+    {
+        await UnitOfWork.Repository<T>().UpdateAsync(entity);
+        await UnitOfWork.CompleteAsync();
+    }
+
+    public async Task DeleteAsync(Guid id)
+    {
+        var entity = await UnitOfWork.Repository<T>().GetByIdAsync(id);
+        if (entity != null)
+        {
+            await UnitOfWork.Repository<T>().DeleteAsync(entity);
+            await UnitOfWork.CompleteAsync();
+        }
+    }
+}
