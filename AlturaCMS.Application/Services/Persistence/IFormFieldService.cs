00e1030f7776f84f8847ab822b33597baa11a1f9
mheidari988 <m.heidari988@gmail.com>
2024-07-25 21:36:12 +0300

    feat: add services for various domain entities
    https://heidari.atlassian.net/browse/CMS-10
    
    - Added ContentTypeService class in AlturaCMS.Application.Services.Persistence namespace
    - Added FormFieldService class in AlturaCMS.Application.Services.Persistence namespace
    - Added FormService class in AlturaCMS.Application.Services.Persistence namespace
    - Added ValidationRulesService class in AlturaCMS.Application.Services.Persistence namespace
    
    These services implement the IBasePersistenceService interface for their respective domain entities, providing CRUD operations and other common persistence functionality.

---
+++ AlturaCMS.Application/Services/Persistence/IFormFieldService.cs
@@ -0,0 +1,8 @@
+﻿using AlturaCMS.Application.Services.Persistence.Common;
+using AlturaCMS.Domain.Entities;
+
+namespace AlturaCMS.Application.Services.Persistence;
+public interface IFormFieldService : IBasePersistenceService<FormField>
+{
+    // Add any additional methods specific to FormFieldService here
+}
---
+++ AlturaCMS.Application/Services/Persistence/IValidationRulesService.cs
@@ -0,0 +1,8 @@
+﻿using AlturaCMS.Application.Services.Persistence.Common;
+using AlturaCMS.Domain.Entities;
+
+namespace AlturaCMS.Application.Services.Persistence;
+public interface IValidationRulesService : IBasePersistenceService<ValidationRules>
+{
+    // Add any additional methods specific to ValidationRulesService here
+}
