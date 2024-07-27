c8d79b476556464926393fd8687d1be3b2c994d9
mheidari988 <m.heidari988@gmail.com>
2024-07-27 13:30:22 +0300

    build: update project dependencies and configure services
    - Added `Microsoft.EntityFrameworkCore.Design` and `Microsoft.EntityFrameworkCore.Tools` with private assets to `.csproj`.
    - Configured `AddPersistence` service in `WebApplication` builder.

--- AlturaCMS.API/Program.cs
+++ AlturaCMS.API/Program.cs
@@ -1,5 +1,8 @@
+using AlturaCMS.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddPersistence(builder.Configuration);
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
