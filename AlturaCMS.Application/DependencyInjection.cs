5bef24d0fc6a83ec6c25fd402d5c9d509c9efa4f
mheidari988 <m.heidari988@gmail.com>
2024-07-27 16:10:01 +0300

    feat: configure dependency injection for Application services
    - Added `AddApplication` method to configure services in `AlturaCMS.Application`.
    - Registered scoped services for `IContentTypeService`, `IFieldService`, `IFormFieldService`, `IFormService`, and `IValidationRulesService`.
    - Converted `DependencyInjection` class to a static class for proper DI configuration.

--- AlturaCMS.Application/DependencyInjection.cs
+++ AlturaCMS.Application/DependencyInjection.cs
@@ -1,10 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AlturaCMS.Application.Services.Persistence;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace AlturaCMS.Application;
-internal class DependencyInjection
+public static class DependencyInjection
 {
+    public static IServiceCollection AddApplication(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddScoped<IContentTypeService, ContentTypeService>();
+        services.AddScoped<IFieldService, FieldService>();
+        services.AddScoped<IFormFieldService, FormFieldService>();
+        services.AddScoped<IFormService, FormService>();
+        services.AddScoped<IValidationRulesService, ValidationRulesService>();
+
+        return services;
+    }
 }
