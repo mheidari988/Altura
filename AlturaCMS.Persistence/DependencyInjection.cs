6f73592dc92f1c8ba87f010dcd25c7418852587b
mheidari988 <m.heidari988@gmail.com>
2024-07-27 12:40:25 +0300

    feat: configure Dependency Injection for Persistence layer
    - Added `AddPersistence` extension method to configure DI for `ApplicationDbContext`, repositories, and unit of work.
    - Removed unused `PersistenceDependencyInjection` class.
    - Updated `AlturaCMS.Application.csproj` to include necessary EF Core packages.

---
+++ AlturaCMS.Persistence/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using AlturaCMS.Persistence.Context;
+using AlturaCMS.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AlturaCMS.Persistence;
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+        services.AddScoped(typeof(IAsyncRepository<>), typeof(EfRepository<>));
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+        return services;
+    }
+}
