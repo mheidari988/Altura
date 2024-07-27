4c7aa4244ebaf11d7cc6e5f9960d65f24a9a8d47
mheidari988 <m.heidari988@gmail.com>
2024-07-27 11:59:42 +0300

    feat: add ContentTypeConfiguration for Entity Framework
    - Introduced `ContentTypeConfiguration` class implementing `IEntityTypeConfiguration<ContentType>`.
    - Configured `ContentType` entity with key and property constraints.
    - Ensured `Name` property is required and has a maximum length of 100 characters.

---
+++ AlturaCMS.Persistence/Configurations/ContentTypeConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using AlturaCMS.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AlturaCMS.Persistence.Configurations;
+public class ContentTypeConfiguration : IEntityTypeConfiguration<ContentType>
+{
+    public void Configure(EntityTypeBuilder<ContentType> builder)
+    {
+        builder.HasKey(e => e.Id);
+        builder.Property(e => e.Name)
+            .IsRequired()
+            .HasMaxLength(100);
+    }
+}
