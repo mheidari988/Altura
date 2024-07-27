b555a700151d4d5db83c997eab4211d35c737c85
mheidari988 <m.heidari988@gmail.com>
2024-07-27 12:01:22 +0300

    feat: add FormConfiguration for Entity Framework
    - Introduced `FormConfiguration` class implementing `IEntityTypeConfiguration<Form>`.
    - Configured `Form` entity with key and property constraints.
    - Ensured `Name` property is required and has a maximum length of 100 characters.

---
+++ AlturaCMS.Persistence/Configurations/FormConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using AlturaCMS.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AlturaCMS.Persistence.Configurations;
+public class FormConfiguration : IEntityTypeConfiguration<Form>
+{
+    public void Configure(EntityTypeBuilder<Form> builder)
+    {
+        builder.HasKey(e => e.Id);
+        builder.Property(e => e.Name)
+            .IsRequired()
+            .HasMaxLength(100);
+    }
+}
