d21f803a91f5ae2c92e6d489901766ede53134c9
mheidari988 <m.heidari988@gmail.com>
2024-07-27 12:02:25 +0300

    feat: add ValidationRulesConfiguration for Entity Framework
    - Introduced `ValidationRulesConfiguration` class implementing `IEntityTypeConfiguration<ValidationRules>`.
    - Configured `ValidationRules` entity with key and property constraints.
    - Set up conversion for `AllowedValues` property to handle list to string conversion.
    - Ensured `RegexPattern` property has a maximum length of 200 characters.

---
+++ AlturaCMS.Persistence/Configurations/ValidationRulesConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using AlturaCMS.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AlturaCMS.Persistence.Configurations;
+public class ValidationRulesConfiguration : IEntityTypeConfiguration<ValidationRules>
+{
+    public void Configure(EntityTypeBuilder<ValidationRules> builder)
+    {
+        builder.HasKey(e => e.Id);
+        builder.Property(e => e.RegexPattern)
+            .HasMaxLength(200);
+        builder.Property(e => e.AllowedValues)
+            .HasConversion(
+                v => string.Join(',', v),
+                v => v.Split(',', StringSplitOptions.RemoveEmptyEntries).ToList());
+    }
+}
