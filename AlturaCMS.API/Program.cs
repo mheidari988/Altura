e1b93070a4ce2afd473aa45d5e11bbe77f912dc7
mheidari988 <m.heidari988@gmail.com>
2024-07-27 16:10:46 +0300

    feat: configure application and persistence services in Program.cs
    - Added `AddApplication` and `AddPersistence` service configurations to `Program.cs`.
    - Registered application and persistence services for dependency injection.

--- AlturaCMS.API/Program.cs
+++ AlturaCMS.API/Program.cs
@@ -1,8 +1,11 @@
+using AlturaCMS.Application;
 using AlturaCMS.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddApplication(builder.Configuration);
 builder.Services.AddPersistence(builder.Configuration);
+
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
