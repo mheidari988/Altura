b3af0d7966395aa48271d37b31fec374783bcf3a
mheidari988 <m.heidari988@gmail.com>
2024-07-27 12:00:53 +0300

    feat: add FieldConfiguration for Entity Framework
    - Introduced `FieldConfiguration` class implementing `IEntityTypeConfiguration<Field>`.
    - Configured `Field` entity with key and property constraints.
    - Set up relationship with `ValidationRules` entity and applied restrict delete behavior.

---
+++ AlturaCMS.Persistence/Configurations/FieldConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using AlturaCMS.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AlturaCMS.Persistence.Configurations;
+public class FieldConfiguration : IEntityTypeConfiguration<Field>
+{
+    public void Configure(EntityTypeBuilder<Field> builder)
+    {
+        builder.HasKey(e => e.Id);
+        builder.Property(e => e.Name)
+            .IsRequired()
+            .HasMaxLength(100);
+        builder.HasOne(e => e.ValidationRules)
+            .WithMany()
+            .HasForeignKey(e => e.ValidationRulesId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
