1a14a3267e5fefd1e1bd11ec0377a7121a9c5eca
mheidari988 <m.heidari988@gmail.com>
2024-07-30 18:32:55 +0300

    feat: add endpoint for retrieving fields
    - Added a new endpoint `/api/fields` to retrieve fields.
    - Implemented `MapFieldsEndpoints` extension method in the `FieldsEndPoints` class.
    - Integrated the new endpoint into the application's HTTP request pipeline by calling `app.MapFieldsEndpoints()`.

---
+++ AlturaCMS.API/Endpoints/FieldsEndPoints.cs
@@ -0,0 +1,16 @@
+﻿using AlturaCMS.Application.Features.Fields.Queries.GetFields;
+using MediatR;
+
+namespace AlturaCMS.API.Endpoints;
+
+public static class FieldsEndPoints
+{
+    public static void MapFieldsEndpoints(this IEndpointRouteBuilder endpoints)
+    {
+        endpoints.MapGet("/api/fields", async (IMediator mediator) =>
+        {
+            var result = await mediator.Send(new GetFieldsQuery());
+            return Results.Ok(result);
+        }).WithName("GetFields").WithOpenApi();
+    }
+}
