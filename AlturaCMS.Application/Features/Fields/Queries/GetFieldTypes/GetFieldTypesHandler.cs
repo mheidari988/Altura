c4165d2b2c9026af111da65e4226b3f7cfaeb048
mheidari988 <m.heidari988@gmail.com>
2024-07-31 17:31:44 +0300

    feat: add GetFieldTypes query and handler
    - Added `GetFieldTypesQuery` to initiate the request for retrieving field types.
    - Implemented `GetFieldTypesHandler` to handle the query, including validation and retrieval of field types.
    - Created `GetFieldTypesResponse` to encapsulate the response data, including a list of `FieldTypeDto`.
    - Defined `FieldTypeDto` to represent field type data with `Id` and `Name` properties.

---
+++ AlturaCMS.Application/Features/Fields/Queries/GetFieldTypes/GetFieldTypesHandler.cs
@@ -0,0 +1,31 @@
+﻿using AlturaCMS.Domain.Entities;
+using FluentValidation;
+using MediatR;
+
+namespace AlturaCMS.Application.Features.Fields.Queries.GetFieldTypes;
+public class GetFieldTypesHandler : IRequestHandler<GetFieldTypesQuery, GetFieldTypesResponse>
+{
+    public async Task<GetFieldTypesResponse> Handle(GetFieldTypesQuery request, CancellationToken cancellationToken)
+    {
+        var validator = new GetFieldTypesValidator();
+        var validationResult = await validator.ValidateAsync(request, cancellationToken);
+        if (!validationResult.IsValid)
+        {
+            throw new ValidationException(validationResult.Errors);
+        }
+
+        var fieldTypes = Enum.GetValues(typeof(FieldType))
+            .Cast<FieldType>()
+            .Select(ft => new FieldTypeDto
+            {
+                Id = (int)ft,
+                Name = ft.ToString()
+            })
+            .ToList();
+
+        return new GetFieldTypesResponse
+        {
+            FieldTypes = fieldTypes
+        };
+    }
+}
