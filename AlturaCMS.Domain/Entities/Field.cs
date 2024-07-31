8bc078853d63a95f87d58b6620413521631e0045
mheidari988 <m.heidari988@gmail.com>
2024-07-31 17:17:40 +0300

    fix: resolve property naming conflicts and nullable list initialization
      - Ensured properties like `Slug`, `Name`, `DisplayName`, `FieldType`, and others have appropriate constraints and configurations.
      - Added value conversion and comparer for the `AllowedValues` list property.
    - Updated `AllowedValues` property to be nullable to avoid initialization issues.

--- AlturaCMS.Domain/Entities/Field.cs
+++ AlturaCMS.Domain/Entities/Field.cs
@@ -12,17 +12,24 @@ namespace AlturaCMS.Domain.Entities
         /// </summary>
         /// <value>The slug of the field.</value>
         public string Slug { get; set; } = string.Empty;
+
         /// <summary>
-        /// Gets or sets the name of the attribute.
+        /// Gets or sets the name of the attribute in the database table.
         /// </summary>
-        /// <value>The name of the field.</value>
+        /// <value>The name of the attribute in table.</value>
         public string Name { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets display name for the field.
+        /// </summary>
+        /// <value>The display name of the field</value>
+        public string DisplayName { get; set; } = string.Empty;
 
         /// <summary>
         /// Gets or sets the type of the field.
         /// </summary>
         /// <value>The type of the field.</value>
-        public FieldType Type { get; set; }
+        public FieldType FieldType { get; set; }
 
         /// <summary>
         /// Gets or sets a value indicating whether the field or form field is required.
@@ -64,6 +71,6 @@ namespace AlturaCMS.Domain.Entities
         /// Gets or sets the list of allowed values for the field or form field.
         /// </summary>
         /// <value>The list of allowed values for the field or form field.</value>
-        public List<string> AllowedValues { get; set; } = [];
+        public List<string>? AllowedValues { get; set; } = [];
     }
 }
