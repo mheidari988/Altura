bb1bd69468f8245dfa2956ba016b0fec12752720
mheidari988 <m.heidari988@gmail.com>
2024-07-31 23:42:34 +0300

    feat: add NVarCharFieldScriptGenerator for generating SQL script for NVARCHAR fields
    - Introduced `NVarCharFieldScriptGenerator` class to generate SQL scripts for fields of type `NVARCHAR`.
    - Implemented `GenerateFieldScript` method to create a script for an `NVARCHAR` field, including constraints for `IsRequired`, `IsUnique`, `MinLength`, `MaxLength`, and `RegexPattern`.
    - Included logic to determine the length of the `NVARCHAR` field, defaulting to `500` if `IsUnique` and no specific `MaxLength` is provided.
    - Added private `AppendConstraints` method to handle the addition of constraints to the SQL script.

---
+++ AlturaCMS.Application/Services/Persistence/Dynamic/NVarCharFieldScriptGenerator.cs
@@ -0,0 +1,52 @@
+﻿using AlturaCMS.Domain.Entities;
+using System.Text;
+
+namespace AlturaCMS.Application.Services.Persistence.Dynamic;
+public class NVarCharFieldScriptGenerator : IFieldScriptGenerator
+{
+    public string GenerateFieldScript(Field field)
+    {
+        var sb = new StringBuilder();
+        string len = "MAX";
+        if (field.IsUnique)
+        {
+            len = field.MaxLength.HasValue ? field.MaxLength.Value.ToString() : "500";
+        }
+        sb.Append($"[{field.Name}] NVARCHAR({len})");
+
+        AppendConstraints(sb, field);
+
+        sb.Append(",");
+        return sb.ToString();
+    }
+
+    private void AppendConstraints(StringBuilder sb, Field field)
+    {
+        var checkConstraints = new List<string>();
+
+        if (field.IsRequired)
+        {
+            sb.Append(" NOT NULL");
+        }
+        if (field.IsUnique)
+        {
+            sb.Append(" UNIQUE");
+        }
+        if (field.MinLength.HasValue)
+        {
+            checkConstraints.Add($"LEN([{field.Name}]) >= {field.MinLength.Value}");
+        }
+        if (field.MaxLength.HasValue)
+        {
+            checkConstraints.Add($"LEN([{field.Name}]) <= {field.MaxLength.Value}");
+        }
+        if (!string.IsNullOrEmpty(field.RegexPattern))
+        {
+            checkConstraints.Add($"[{field.Name}] LIKE '{field.RegexPattern}'");
+        }
+        if (checkConstraints.Count > 0)
+        {
+            sb.Append(" CHECK (" + string.Join(" AND ", checkConstraints) + ")");
+        }
+    }
+}
