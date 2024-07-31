2bb53042a4fde1395189b07decbb4eceb0eebfdf
mheidari988 <m.heidari988@gmail.com>
2024-07-31 23:46:50 +0300

    refactor: enhance CreateContentTypeHandler with unit of work and transaction management
    - Enhanced `CreateContentTypeHandler` to include unit of work and transaction management.
    - Injected `IUnitOfWork` into the handler to manage transactions.
    - Began a transaction at the start of the handler and committed or rolled back based on the success of content type and table creation.
    - Added a required "Slug" field to each content type, ensuring it is unique and properly configured.
    - Improved error handling to ensure transactions are rolled back in case of failures.
    - Simplified field creation and association with content type within the transaction scope.

--- AlturaCMS.Application/Features/ContentTypes/Commands/CreateContentType/CreateContentTypeHandler.cs
+++ AlturaCMS.Application/Features/ContentTypes/Commands/CreateContentType/CreateContentTypeHandler.cs
@@ -1,77 +1,120 @@
 ﻿using AlturaCMS.Application.Services.Persistence;
 using AlturaCMS.Domain.Entities;
+using AlturaCMS.Persistence.Repositories;
 using FluentValidation;
 using MediatR;
 
-namespace AlturaCMS.Application.Features.ContentTypes.Commands.CreateContentType;
-public class CreateContentTypeHandler(
-    IContentTypeService contentTypeService,
-    IFieldService fieldService,
-    IDynamicTableService dynamicTableService) : IRequestHandler<CreateContentTypeCommand, CreateContentTypeResponse>
+namespace AlturaCMS.Application.Features.ContentTypes.Commands.CreateContentType
 {
-    public async Task<CreateContentTypeResponse> Handle(CreateContentTypeCommand request, CancellationToken cancellationToken)
+    public class CreateContentTypeHandler : IRequestHandler<CreateContentTypeCommand, CreateContentTypeResponse>
     {
-        var validator = new CreateContentTypeValidator();
-        var validationResult = await validator.ValidateAsync(request, cancellationToken);
-        if (!validationResult.IsValid)
+        private readonly IContentTypeService _contentTypeService;
+        private readonly IFieldService _fieldService;
+        private readonly IDynamicTableService _dynamicTableService;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CreateContentTypeHandler(
+            IContentTypeService contentTypeService,
+            IFieldService fieldService,
+            IDynamicTableService dynamicTableService,
+            IUnitOfWork unitOfWork)
         {
-            throw new ValidationException(validationResult.Errors);
+            _contentTypeService = contentTypeService;
+            _fieldService = fieldService;
+            _dynamicTableService = dynamicTableService;
+            _unitOfWork = unitOfWork;
         }
 
-        var contentType = new ContentType
+        public async Task<CreateContentTypeResponse> Handle(CreateContentTypeCommand request, CancellationToken cancellationToken)
         {
-            Name = request.Name,
-            Fields = []
-        };
-
-        request.Fields.Add(new CreateContentTypeFieldDto()
-        {
-            Name = "Slug",
-            DisplayName = "Slug",
-            FieldType = FieldType.Text,
-            IsRequired = true,
-            MaxLength = 255,
-        });
-
-        foreach (var fieldDto in request.Fields)
-        {
-            // create field
-            var field = await fieldService.CreateAsync(new Field
+            var validator = new CreateContentTypeValidator();
+            var validationResult = await validator.ValidateAsync(request, cancellationToken);
+            if (!validationResult.IsValid)
             {
-                Name = fieldDto.Name,
-                DisplayName = fieldDto.DisplayName,
-                FieldType = fieldDto.FieldType,
-                IsRequired = fieldDto.IsRequired,
-                AllowedValues = fieldDto.AllowedValues,
-                MaxLength = fieldDto.MaxLength,
-                MinLength = fieldDto.MinLength,
-                MaxValue = fieldDto.MaxValue,
-                MinValue = fieldDto.MinValue,
-                RegexPattern = fieldDto.RegexPattern,
-            });
+                throw new ValidationException(validationResult.Errors);
+            }
 
-            if (field != null)
+            await _unitOfWork.BeginTransactionAsync();
+
+            try
             {
-                contentType.Fields.Add(new ContentTypeField
+                var contentType = new ContentType
                 {
-                    FieldId = field.Id,
-                    Field = field
+                    Name = request.Name,
+                    Fields = []
+                };
+
+                // Slug is a required field for all content types
+                request.Fields.Insert(0, new CreateContentTypeFieldDto()
+                {
+                    Name = "Slug",
+                    DisplayName = "Slug",
+                    FieldType = FieldType.Text,
+                    IsRequired = true,
+                    IsUnique = true,
+                    MaxLength = 500,
+                    RegexPattern = null
                 });
+
+                foreach (var fieldDto in request.Fields)
+                {
+                    var field = await _fieldService.CreateAsync(new Field
+                    {
+                        Name = fieldDto.Name,
+                        DisplayName = fieldDto.DisplayName,
+                        FieldType = fieldDto.FieldType,
+                        IsRequired = fieldDto.IsRequired,
+                        IsUnique = fieldDto.IsUnique,
+                        AllowedValues = fieldDto.AllowedValues,
+                        MaxLength = fieldDto.MaxLength,
+                        MinLength = fieldDto.MinLength,
+                        MaxValue = fieldDto.MaxValue,
+                        MinValue = fieldDto.MinValue,
+                        RegexPattern = fieldDto.RegexPattern,
+                    });
+
+                    if (field != null)
+                    {
+                        contentType.Fields.Add(new ContentTypeField
+                        {
+                            FieldId = field.Id,
+                            Field = field
+                        });
+                    }
+                }
+
+                var createdContentType = await _contentTypeService.CreateAsync(contentType)
+                    ?? throw new Exception("Failed to create content type");
+
+                var isCreated = await _dynamicTableService.CreateTableAsync(createdContentType);
+
+                if (createdContentType != null && isCreated)
+                {
+                    await _unitOfWork.CompleteAsync();
+                    await _unitOfWork.CommitTransactionAsync();
+                }
+                else
+                {
+                    await _unitOfWork.RollbackTransactionAsync();
+                    throw new Exception("Transaction failed: either content type creation or table creation failed.");
+                }
+
+                return new CreateContentTypeResponse
+                {
+                    Id = createdContentType.Id,
+                    Name = createdContentType.Name,
+                    Fields = createdContentType.Fields.Select(f => new ContentTypeFieldDto
+                    {
+                        FieldId = f.FieldId ?? Guid.Empty,
+                        FieldName = f.Field?.Name?.ToString() ?? string.Empty,
+                    }).ToList()
+                };
+            }
+            catch (Exception)
+            {
+                await _unitOfWork.RollbackTransactionAsync();
+                throw;
             }
         }
-
-        var createdContentType = await contentTypeService.CreateAsync(contentType) ?? throw new Exception("Failed to create content type");
-        await dynamicTableService.CreateTableAsync(createdContentType);
-
-        return new CreateContentTypeResponse
-        {
-            Id = createdContentType.Id,
-            Name = createdContentType.Name,
-            Fields = createdContentType.Fields.Select(f => new ContentTypeFieldDto
-            {
-                FieldId = f.FieldId ?? Guid.Empty,
-                FieldName = f.Field?.Name?.ToString() ?? string.Empty
-            }).ToList()
-        };
     }
 }
