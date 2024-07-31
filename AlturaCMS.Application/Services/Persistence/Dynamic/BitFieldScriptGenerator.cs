c997cb5285d4e44beee503dc9f85f293dff1c1d3
mheidari988 <m.heidari988@gmail.com>
2024-07-31 23:44:08 +0300

    feat: add BitFieldScriptGenerator for generating SQL script for BIT fields
    - Introduced `BitFieldScriptGenerator` class to generate SQL scripts for fields of type `BIT`.
    - Implemented `GenerateFieldScript` method to create a script for a `BIT` field, including constraints for `IsRequired` and `IsUnique`.
    - Ensured the generated script includes `NOT NULL` if the field is required and `UNIQUE` if the field must be unique.

---
+++ AlturaCMS.Application/Services/Persistence/Dynamic/BitFieldScriptGenerator.cs
@@ -0,0 +1,25 @@
+﻿using AlturaCMS.Domain.Entities;
+using System.Text;
+
+namespace AlturaCMS.Application.Services.Persistence.Dynamic;
+public class BitFieldScriptGenerator : IFieldScriptGenerator
+{
+    public string GenerateFieldScript(Field field)
+    {
+        var sb = new StringBuilder();
+        sb.Append($"[{field.Name}] BIT");
+
+        if (field.IsRequired)
+        {
+            sb.Append(" NOT NULL");
+        }
+
+        if (field.IsUnique)
+        {
+            sb.Append(" UNIQUE");
+        }
+
+        sb.Append(",");
+        return sb.ToString();
+    }
+}
