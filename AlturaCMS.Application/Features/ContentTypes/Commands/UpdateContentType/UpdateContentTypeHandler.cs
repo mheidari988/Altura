805c3878c2d17af4d290beeb90751fd1768482d4
mheidari988 <m.heidari988@gmail.com>
2024-08-01 17:57:20 +0300

    feat: implement UpdateContentType feature with handler and validator
    - Added `UpdateContentTypeCommand` class implementing `IRequest<UpdateContentTypeResponse>` in the `AlturaCMS.Application.Features.ContentTypes.Commands.UpdateContentType` namespace.
    - Implemented `UpdateContentTypeHandler` class inheriting `IRequestHandler<UpdateContentTypeCommand, UpdateContentTypeResponse>`, including a `Handle` method with a `NotImplementedException` placeholder.
    - Added `UpdateContentTypeValidator` class extending `AbstractValidator<UpdateContentTypeCommand>` for validating the command.

--- AlturaCMS.Application/Features/ContentTypes/Commands/UpdateContentType/UpdateContentTypeHandler.cs
+++ AlturaCMS.Application/Features/ContentTypes/Commands/UpdateContentType/UpdateContentTypeHandler.cs
@@ -1,10 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MediatR;
 
 namespace AlturaCMS.Application.Features.ContentTypes.Commands.UpdateContentType;
-public class UpdateContentTypeHandler
+public class UpdateContentTypeHandler : IRequestHandler<UpdateContentTypeCommand, UpdateContentTypeResponse>
 {
+    public Task<UpdateContentTypeResponse> Handle(UpdateContentTypeCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
 }
