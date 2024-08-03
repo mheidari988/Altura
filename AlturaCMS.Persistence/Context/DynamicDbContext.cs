78dd26f0bc6ce0e1a72fc57a009572ec03bed12f
mheidari988 <m.heidari988@gmail.com>
2024-08-03 14:41:59 +0300

    refactor: remove DynamicDbContext class
    Removed the `DynamicDbContext` class from the codebase. This class previously provided a configurable EF Core context using a connection string and a model configuration action for DynamicTableService. The removal is the part of a larger refactoring effort to streamline the persistence layer and migrate to a different approach for handling database in DynamicTableService.

--- AlturaCMS.Persistence/Context/DynamicDbContext.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.EntityFrameworkCore;
-
-namespace AlturaCMS.Persistence.Context;
-public class DynamicDbContext(string connectionString, Action<ModelBuilder> configureModel) : DbContext
-{
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlServer(connectionString);
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder) => configureModel(modelBuilder);
-}
