cd5a61cc662f576329a6cab1fde84e4c3d98d421
mheidari988 <m.heidari988@gmail.com>
2024-08-04 14:02:22 +0300

    feat: add DomainShared constants for default fields
    Added a static `DomainShared` class to define constant values for default fields used throughout the application. This includes field names, display names, and maximum length values for entities like Slug, CreatedDate, CreatedBy, UpdatedDate, UpdatedBy, IsDeleted, DeletedDate, DeletedBy, IsActive, and RowVersion. These constants will help standardize field properties and improve consistency across different parts of the application.

---
+++ AlturaCMS.Domain/DomainShared.cs
@@ -0,0 +1,73 @@
+﻿namespace AlturaCMS.Domain;
+public static class DomainShared
+{
+    public static class Constants
+    {
+        public static class DefaultFields
+        {
+            public static class Slug
+            {
+                public const string Name = "Slug";
+                public const string DisplayName = "Slug";
+                public const int MaxLength = 500;
+            }
+
+            public static class CreatedDate
+            {
+                public const string Name = "CreatedDate";
+                public const string DisplayName = "Created Date";
+            }
+
+            public static class CreatedBy
+            {
+                public const string Name = "CreatedBy";
+                public const string DisplayName = "Created By";
+                public const int MaxLength = 500;
+            }
+
+            public static class UpdatedDate
+            {
+                public const string Name = "UpdatedDate";
+                public const string DisplayName = "Updated Date";
+            }
+
+            public static class UpdatedBy
+            {
+                public const string Name = "UpdatedBy";
+                public const string DisplayName = "Updated By";
+                public const int MaxLength = 500;
+            }
+
+            public static class IsDeleted
+            {
+                public const string Name = "IsDeleted";
+                public const string DisplayName = "Is Deleted";
+            }
+
+            public static class DeletedDate
+            {
+                public const string Name = "DeletedDate";
+                public const string DisplayName = "Deleted Date";
+            }
+
+            public static class DeletedBy
+            {
+                public const string Name = "DeletedBy";
+                public const string DisplayName = "Deleted By";
+                public const int MaxLength = 500;
+            }
+
+            public static class IsActive
+            {
+                public const string Name = "IsActive";
+                public const string DisplayName = "Is Active";
+            }
+
+            public static class RowVersion
+            {
+                public const string Name = "RowVersion";
+                public const string DisplayName = "Row Version";
+            }
+        }
+    }
+}
