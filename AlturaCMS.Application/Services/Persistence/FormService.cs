ef152a08ec1e7c0932911d6cc779b084e007e121
mheidari988 <m.heidari988@gmail.com>
2024-08-18 23:35:04 +0300

    refactor: update service classes to inherit correctly from BasePersistenceService with logger support
    - Refactored `FieldService`, `FormFieldService`, and `FormService` to fix inheritance syntax and include `ILogger` support.

--- AlturaCMS.Application/Services/Persistence/FormService.cs
+++ AlturaCMS.Application/Services/Persistence/FormService.cs
@@ -2,10 +2,10 @@
 using AlturaCMS.DataAccess;
 using AlturaCMS.Domain.Entities;
 using AlturaCMS.Persistence.Context;
+using Microsoft.Extensions.Logging;
 
 namespace AlturaCMS.Application.Services.Persistence;
-public class FormService(IUnitOfWork<ApplicationDbContext> unitOfWork) : BasePersistenceService<Form, ApplicationDbContext>(unitOfWork), IFormService
+public class FormService(IUnitOfWork<ApplicationDbContext> unitOfWork, ILogger<BasePersistenceService<Form, ApplicationDbContext>> logger) : BasePersistenceService<Form, ApplicationDbContext>(unitOfWork, logger), IFormService
 {
-
-    // Add any additional methods specific to FormService to the IFormService and implement it here
+    // Add any additional methods specific to this service to the interface and then implement them here
 }
