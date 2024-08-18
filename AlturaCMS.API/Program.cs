4faa8bdb4e7f039e9c1e772920c828b3a84c1283
mheidari988 <m.heidari988@gmail.com>
2024-08-18 23:30:26 +0300

    feat: add Serilog for logging to file and configure in API
    - Added Serilog packages to both AlturaCMS.API and AlturaCMS.Application projects.
    - Configured logging to file (`altura.log`) and console in `Program.cs`.
    - Enabled request logging and exception handling with Serilog.

--- AlturaCMS.API/Program.cs
+++ AlturaCMS.API/Program.cs
@@ -1,11 +1,20 @@
 using AlturaCMS.API.Endpoints;
 using AlturaCMS.Application;
 using AlturaCMS.Persistence;
+using Microsoft.AspNetCore.Diagnostics;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
+Log.Logger = new LoggerConfiguration()
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .WriteTo.File("logs/altura.log", rollingInterval: RollingInterval.Day)
+        .Enrich.WithProperty("ApplicationName", "AlturaCMS")
+        .Enrich.WithProperty("Environment", builder.Environment.EnvironmentName)
+    .CreateLogger();
+builder.Host.UseSerilog();
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
-
 builder.Services.AddAlturaPersistence(builder.Configuration);
 builder.Services.AddAlturaApplication(builder.Configuration);
 
@@ -15,16 +24,26 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
+app.UseSerilogRequestLogging();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 app.UseHttpsRedirection();
-
-app.MapContentTypesEndpoints();
+app.MapContentsEndpoints();
 app.MapFieldsEndpoints();
+app.UseExceptionHandler(errorApp =>
+{
+    errorApp.Run(async context =>
+    {
+        var errorFeature = context.Features.Get<IExceptionHandlerFeature>();
+        if (errorFeature != null)
+        {
+            Log.Error(errorFeature.Error, "Unhandled exception occurred.");
+            await context.Response.WriteAsync("An error occurred, please try again later.");
+        }
+    });
+});
 
 app.Run();
